f3b01f522d3c7342ad1d839e15e768d37efc44df
Rahman Saputra <rahman@artikulpi.com>
2016-03-31 19:17:18 +0700

    modif map

--- application/views/template/layout.php
+++ application/views/template/layout.php
@@ -66,14 +66,14 @@
     
     <!-- /Section: intro -->
     <!-- Navigation -->
-<section id="home">
+    <section id="home">
 
-    
+        
         <div id="background_image">
             <img src="<?php echo base_url('media/img/buatdewo_copy.jpg') ?>" alt="">
         </div> 
 
-    <div id="navigation">
+        <div id="navigation">
         <!--
         <img src="#" alt="Icon"> -->
         
@@ -82,7 +82,7 @@
             <div class = "navbar-header">
                 <a class = "navbar-brand page-scroll" href = "#" >AGUS & ISMA</a>
 
-            <button type = "button" class = "navbar-toggle" 
+                <button type = "button" class = "navbar-toggle" 
                 data-toggle = "collapse" data-target = "#example-navbar-collapse">
                 <span class = "sr-only">Toggle navigation</span>
                 <span class = "icon-bar"></span>
@@ -101,96 +101,96 @@
             </ul>
         </div>
     </nav>
-    </div>
+</div>
 </section> 
 
 <!-- /Navigation -->  
 
-    <!-- Section: Story -->
-    <section id="story" class="home-section text-center bg-gray">
+<!-- Section: Story -->
+<section id="story" class="home-section text-center bg-gray">
 
-     
-      <div class="heading-about">
-        <div class="container marginbot-50">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <div class="section-heading">
-                        <h1>Story</h1>
-                        <!--<p>We should remember the differences that brought us here and keep alive the memories of what YOU & I in persuite of what we can become together</p> -->
-                        <div class="img_deco">
+   
+  <div class="heading-about">
+    <div class="container marginbot-50">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <div class="section-heading">
+                    <h1>Story</h1>
+                    <!--<p>We should remember the differences that brought us here and keep alive the memories of what YOU & I in persuite of what we can become together</p> -->
+                    <div class="img_deco">
                         <img src="media/img/tes_3.png" alt="">
-                        </div>
                     </div>
-
                 </div>
+
             </div>
         </div>
     </div>
-        <div class="section-content">
-          <div class="story-timeline">
+</div>
+<div class="section-content">
+  <div class="story-timeline">
+    <div class="row">
+      <div class="each-story">
+        <div class="col-md-4">
+          <h2 class="story-date p-sm-20 mt-sm-0" ><em><b> September 2010</b></em></h2>
+      </div>
+      <div class="col-md-8">
+          <div class="story-details">
             <div class="row">
-              <div class="each-story">
-                <div class="col-md-4">
-                  <h2 class="story-date p-sm-20 mt-sm-0" ><em><b> September 2010</b></em></h2>
-                </div>
-                <div class="col-md-8">
-                  <div class="story-details">
-                    <div class="row">
-                      <div class="col-sm-9">
-                        <h4>First meet</h4>
+              <div class="col-sm-9">
+                <h4>First meet</h4>
 
-                        <p>"Mata kami bertemu untuk pertama kali, tak ada rasa, tak ada asa, hanya sedikit rasa penasaran, sibuk dengan cinta masing - masing, berjalan dengan arah berkelok, namun selalu bertemu pada satu titik"</p>
+                <p>"Mata kami bertemu untuk pertama kali, tak ada rasa, tak ada asa, hanya sedikit rasa penasaran, sibuk dengan cinta masing - masing, berjalan dengan arah berkelok, namun selalu bertemu pada satu titik"</p>
 
-                      </div>
-                      <div class="col-sm-3">
-                        <div class="story-thumb">
-                          <img class="img-box" src="media/img/edit2.jpg" alt="">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="story-thumb">
+                  <img class="img-box" src="media/img/edit2.jpg" alt="">
               </div>
-              <div class="each-story">
-                <div class="col-md-4 pull-right">
-                  <h2 class="story-date p-sm-20 mt-sm-0"><em><b> November 2012</b></em></h2>
-                </div>
-                <div class="col-md-8">
-                  <div class="story-details">
-                    <div class="row">
-                      <div class="col-sm-9">
-                        <h4 class="title">First date</h4>
-                        <p>"Di bengkel tua, hujan menjadi saksi pernyataan yang mengubah titik menjadi rangkaian ritme nada indah, cukup kata 'kita', setiap warna-warninya terasa bermakna"</p>
-                      </div>
-                      <div class="col-sm-3">
-                        <div class="story-thumb">
-                          <img class="img-box" src="media/img/buatdewo.jpg" alt="">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="each-story">
-                <div class="col-md-4">
-                  <h2 class="story-date p-sm-20 mt-sm-0"><em><b> Januari 2016 </b></em></h2>
-                </div>
-                <div class="col-md-8">
-                  <div class="story-details">
-                    <div class="row">
-                      <div class="col-sm-9">
-                        <h4>Proposed</h4>
-                        <p>"Kilauan asa masa depan sudi bertandang di jari kami, mengharap ridho ilahi untuk melepaskan satu persatu mimpi, mengubah ritme nada menjadi sebuah lagu cinta untuk setiap hembusan nafas dan anugerah dari-Nya"</p>
-                      </div>
-                      <div class="col-sm-3">
-                        <div class="story-thumb">
-                          <img class="img-box" src="media/img/cetak1.jpg" alt="">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          </div>
+      </div>
+  </div>
+</div>
+</div>
+<div class="each-story">
+    <div class="col-md-4 pull-right">
+      <h2 class="story-date p-sm-20 mt-sm-0"><em><b> November 2012</b></em></h2>
+  </div>
+  <div class="col-md-8">
+      <div class="story-details">
+        <div class="row">
+          <div class="col-sm-9">
+            <h4 class="title">First date</h4>
+            <p>"Di bengkel tua, hujan menjadi saksi pernyataan yang mengubah titik menjadi rangkaian ritme nada indah, cukup kata 'kita', setiap warna-warninya terasa bermakna"</p>
+        </div>
+        <div class="col-sm-3">
+            <div class="story-thumb">
+              <img class="img-box" src="media/img/buatdewo.jpg" alt="">
+          </div>
+      </div>
+  </div>
+</div>
+</div>
+</div>
+<div class="each-story">
+    <div class="col-md-4">
+      <h2 class="story-date p-sm-20 mt-sm-0"><em><b> Januari 2016 </b></em></h2>
+  </div>
+  <div class="col-md-8">
+      <div class="story-details">
+        <div class="row">
+          <div class="col-sm-9">
+            <h4>Proposed</h4>
+            <p>"Kilauan asa masa depan sudi bertandang di jari kami, mengharap ridho ilahi untuk melepaskan satu persatu mimpi, mengubah ritme nada menjadi sebuah lagu cinta untuk setiap hembusan nafas dan anugerah dari-Nya"</p>
+        </div>
+        <div class="col-sm-3">
+            <div class="story-thumb">
+              <img class="img-box" src="media/img/cetak1.jpg" alt="">
+          </div>
+      </div>
+  </div>
+</div>
+</div>
+</div>
                 <!--<div class="col-md-12">
                     <div class="video">
                         <video autoplay loop>
@@ -198,12 +198,12 @@
                         </video>
                     </div>
                 </div>
-                 <div class="block"></div>-->
+                <div class="block"></div>-->
             </div>
-          </div>
         </div>
+    </div>
 
-       
+    
       <!--
       <div id="intro_1" class="intro_1">
             <div id="background_bottom" >
@@ -219,23 +219,23 @@
                 </span>
             </div>
         </div>
-        </div>-->
-    </section> 
+    </div>-->
+</section> 
 
-    <!-- /Section: about -->
+<!-- /Section: about -->
 
-    <!-- Section: services -->
-    <section id="when" class="">
-        <div class="container">
+<!-- Section: services -->
+<section id="when" class="">
+    <div class="container">
         <div class="container marginbot-50">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2">
                     <div>
                         <div class="section-heading text-center">
-                        <div class="when-head">
-                            <h2>When</h2>
-                            <img src="media/img/tes_3.png" alt="">
-                          </div>
+                            <div class="when-head">
+                                <h2>When</h2>
+                                <img src="media/img/tes_3.png" alt="">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -243,105 +243,104 @@
         </div>
 
         
-            <div class="row">
-                <div class="col-md-4  feature text-center">
-                    <h3><b>Akad Nikah</b></h3>
-                    <div class="feature-content">
-                        <h5>Pukul 09.00 WIB</h5>
-                        <p>Bertempat di kediaman mempelai wanita <span>Ds. Tlajung Udik RT 02 RW 07 No. 59. Kec. Gunung Putri, Kab. Bogor</span></p>
+        <div class="row">
+            <div class="col-md-4  feature text-center">
+                <h3><b>Akad Nikah</b></h3>
+                <div class="feature-content">
+                    <h5>Pukul 09.00 WIB</h5>
+                    <p>Bertempat di kediaman mempelai wanita <span>Ds. Tlajung Udik RT 02 RW 07 No. 59. Kec. Gunung Putri, Kab. Bogor</span></p>
+                </div>
+            </div>
+            <div class="col-lg-4 feature text-center" id="date_1">
+                <img src="media/img/tes_6.png" alt="">
+                <p><b> Minggu</b></p>
+                <h1>10</h1>
+                <p><b> April 2016</b></p>
+                <img src="media/img/tes_5.png" alt="" class="test_1">
+            </div>
+            <div class="col-md-4 feature text-center">
+                <h3><b>Resepsi Pernikahan</b></h3>
+                <div class="feature-content">
+                    <h5>Pukul 11.00 WIB s/d Selesai</h5>
+                    <p>Bertempat di kediaman mempelai wanita
+                        <span>Ds. Tlajung Udik RT 02 RW 07 No. 59. Kec. Gunung Putri, Kab. Bogor</span>
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section> 
+<!-- /Section: services -->
+
+
+<!-- Section: contact -->
+<section id="Location">
+    <div class="container">
+        <div class="loc-test">
+            <div class="heading-contact marginbot-50">
+               <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    
+                    <div class="section-heading text-center">
+                        <h2>Location</h2>
+                        <img src="media/img/tes_3.png" alt="">
+                        <p></p>
                     </div>
+                    
                 </div>
-               <div class="col-lg-4 feature text-center" id="date_1">
-                    <img src="media/img/tes_6.png" alt="">
-                    <p><b> Minggu</b></p>
-                    <h1>10</h1>
-                    <p><b> April 2016</b></p>
-                    <img src="media/img/tes_5.png" alt="" class="test_1">
-                </div>
-                <div class="col-md-4 feature text-center">
-                    <h3><b>Resepsi Pernikahan</b></h3>
-                    <div class="feature-content">
-                        <h5>Pukul 11.00 WIB s/d Selesai</h5>
-                        <p>Bertempat di kediaman mempelai wanita
-                            <span>Ds. Tlajung Udik RT 02 RW 07 No. 59. Kec. Gunung Putri, Kab. Bogor</span>
-                        </p>
-                    </div>
-                </div>
-              
             </div>
         </div>
-     </section> 
-        <!-- /Section: services -->
 
-
-        <!-- Section: contact -->
-        <section id="Location">
-            <div class="container">
-                <div class="loc-test">
-            <div class="heading-contact marginbot-50">
-                 <div class="row">
-                        <div class="col-lg-8 col-lg-offset-2">
-                        
-                            <div class="section-heading text-center">
-                                <h2>Location</h2>
-                                <img src="media/img/tes_3.png" alt="">
-                                <p></p>
-                            </div>
-                        
-                        </div>
-                </div>
-            </div>
-
-           <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 ">
-                         <div id="Navigate">
-                           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.5811418182634!2d106.9033163149092!3d-6.447786664843096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMjYnNTIuMCJTIDEwNsKwNTQnMTkuOCJF!5e0!3m2!1sen!2sid!4v1458742411195" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
-                        <div class="btn">
-                        <a href="https://https://www.google.co.id/maps/place/6%C2%B026'52.0%22S+106%C2%B054'19.8%22E/@-6.4477867,106.9033163,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0?hl=en">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 ">
+                   <div id="Navigate">
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d3924.4185541781603!2d106.90578972486956!3d-6.448927979688218!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d-6.4478007999999996!2d106.90543009999999!5e0!3m2!1sen!2sid!4v1459403020444" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                 </div>
+                 <div class="btn">
+                    <a href="https://www.google.co.id/maps/dir//-6.4478008,106.9054301/@-6.448928,106.9057897,17z?hl=en">
                         <button id="get_nav">Get Navigation</button>
-                        </a>
-                        </div>
-                    </div>
+                    </a>
                 </div>
             </div>
-            </div>
-            </div>
-        </section> 
-        <!-- /Section: contact -->
+        </div>
+    </div>
+</div>
+</section> 
+<!-- /Section: contact -->
 
-        <footer id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-lg-12">
-                        <p>Copyright &copy; 2016  - by <a href="http://rumahcg.com" target="_blank">rumahCG.com</a></p>
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <p>Copyright &copy; 2016  - by <a href="http://rumahcg.com" target="_blank">rumahCG.com</a></p>
                     <!--</div>
                 
                     All links in the footer should remain intact. 
                     Licenseing information is available at: http://bootstraptaste.com/license/
                     You can buy this theme without footer links online at: http://bootstraptaste.com/buy/?theme=Ninestars
                 
-            </div>-->   
-        </div> 
-    </footer>
+                </div>-->   
+            </div> 
+        </footer>
 
-  
-    <!-- Core JavaScript Files -->
-    <script src="media/js/js/jquery.min.js"></script>  
-    <script src="media/js/js/bootstrap.min.js"></script>
-    <script src="media/js/js/jquery.sticky.js"></script>
-    <script src="media/js/js/jquery.flexslider-min.js"></script>
-    <script src="media/js/js/jquery.easing.min.js"></script>  
-    <script src="media/js/js/jquery.scrollTo.js"></script>
-    <script src="media/js/js/jquery.appear.js"></script>
-    <script src="media/js/js/stellar.js"></script>
-    <script src="media/js/js/wow.min.js"></script>
-    <script src="media/js/js/owl.carousel.min.js"></script>
-    <script src="media/js/js/nivo-lightbox.min.js"></script>
+        
+        <!-- Core JavaScript Files -->
+        <script src="media/js/js/jquery.min.js"></script>  
+        <script src="media/js/js/bootstrap.min.js"></script>
+        <script src="media/js/js/jquery.sticky.js"></script>
+        <script src="media/js/js/jquery.flexslider-min.js"></script>
+        <script src="media/js/js/jquery.easing.min.js"></script>  
+        <script src="media/js/js/jquery.scrollTo.js"></script>
+        <script src="media/js/js/jquery.appear.js"></script>
+        <script src="media/js/js/stellar.js"></script>
+        <script src="media/js/js/wow.min.js"></script>
+        <script src="media/js/js/owl.carousel.min.js"></script>
+        <script src="media/js/js/nivo-lightbox.min.js"></script>
 
-    <script src="media/js/js/custom.js"></script>
+        <script src="media/js/js/custom.js"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
